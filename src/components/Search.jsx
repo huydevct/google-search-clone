852b051ba04c02e82792dbbbb1c6acaa81af2acd
huydevct <huyctdev@example.com>
2022-01-17 16:50:48 +0700

    changed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,33 +1,37 @@
-import React,{useEffect, useState} from 'react'
-import { useDebounce } from 'use-debounce';
+import React, { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
 
-import { useResultContext } from '../contexts/ResultContextProvider';
-import { Links } from './Links'
+import { useResultContext } from "../contexts/ResultContextProvider";
+import { Links } from "./Links";
 
 export const Search = () => {
-    const [text, setText] = useState('Do Mixi');
-    const {setSearchTerm} = useResultContext();
-    const [deboucedValue] = useDebounce(text, 300);
+  const [text, setText] = useState("Do Mixi");
+  const { setSearchTerm } = useResultContext();
+  const [deboucedValue] = useDebounce(text, 300);
 
-    useEffect(() => {
-        if(deboucedValue) setSearchTerm(deboucedValue);
-    }, [deboucedValue])
-    
-    return (
-        <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
-            <input 
-                value={text}
-                type="text"
-                className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
-                placeholder="Search Google or type URL"
-                onChange={(e) => setText(e.target.value)}
-            />
-            {!text && (
-                <button type="button" className="absolute top-1.5 right-4 text-2x1 text-gray-500" onClick={() => setText('')}>
-                    X
-                </button>
-            )}
-            <Links />
-        </div>
-    )
-}
+  useEffect(() => {
+    if (deboucedValue) setSearchTerm(deboucedValue);
+  }, [deboucedValue]);
+
+  return (
+    <div className="relative sm:ml-48 md:ml-72 sm:-mt-10 mt-3">
+      <input
+        value={text}
+        type="text"
+        className="sm:w-96 w-80 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
+        placeholder="Search Google or type URL"
+        onChange={(e) => setText(e.target.value)}
+      />
+      {text && (
+        <button
+          type="button"
+          className="absolute top-3.5 right-4 text-2x1 text-gray-500"
+          onClick={() => setText("")}
+        >
+          X
+        </button>
+      )}
+      <Links />
+    </div>
+  );
+};
